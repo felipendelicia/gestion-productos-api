b7c0a2467013ba0bd2d78e7019375202a9cd368e
felipendelicia <delicia4581@gmail.com>
2022-07-14 18:20:21 -0300

    feat: token body

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -5,32 +5,46 @@ import config from "../config/config";
 import jwt from "jsonwebtoken";
 
 export async function login(req: Request, res: Response) {
+  try {
 
-  // Connecting the db
-  const conn = await connectDB();
+    console.log(req.body)
+    // Connecting the db
+    const conn = await connectDB();
 
-  // Get user input
-  const { email, password } = req.body;
+    // Get user input
+    const { email, password } = req.body;
 
-  // Validate user input
-  if (!(email && password)) {
-    res.status(400).send("Todas las entradas son requeridas.");
-  }
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("Todas las entradas son requeridas.");
+    }
 
-  // Validate if user exist in our database
-  const mysql_response = await conn.query<RowDataPacket[]>(
-    `SELECT * FROM accounts WHERE email = ? ;`,
-    [email]
-  );
+    // Validate if user exist in our database
+    const mysql_response = await conn.query<RowDataPacket[]>(
+      `SELECT * FROM accounts WHERE email = ? ;`,
+      [email]
+    );
 
-  const user = mysql_response[0][0];
+    const user = mysql_response[0][0];
 
-  if (user && password == user.password) {
-    // Create token
-    const token = jwt.sign({ id: user.id }, config.TOKEN_KEY!, {expiresIn: "2h"});
-    // user
-    res.header('token', token).status(200).json(user);
-  } else {
-    res.status(400).send("Credenciales invalidas");
+    if (user === undefined) { res.status(404).send('Usuario no encontrado') }
+    console.log(user)
+
+    if (user && password == user.password) {
+      // Create token
+      const token = jwt.sign({ id: user.id }, config.TOKEN_KEY!, {
+        expiresIn: "2h",
+      });
+
+      user.token = token
+      
+      // user
+      res.status(200).json(user);
+    }
+
+  } catch (err) {
+
+    console.error(err);
+
   }
 }
